976b4439158ed59045570db9312782e2b724e8b6
ifBars <114284668+ifBars@users.noreply.github.com>
2023-01-15 12:45:42 -0800

    v1.3.0
    Full Changelog and Usage Docs coming soon...

---
+++ VRChat Error Bot/CommonWords.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRChat_Error_Bot
+{
+    public class CommonWords
+    {
+        public string[] common_words { get; set; }
+    }
+}
--- VRChat Error Bot/Program.cs
+++ VRChat Error Bot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,7 +11,6 @@ namespace VRChat_Error_Bot
 {
     class Program
     {
-
         static Version v = new Version();
 
         static async Task Main(string[] args)
@@ -31,10 +31,32 @@ namespace VRChat_Error_Bot
                 var assembly = Assembly.GetExecutingAssembly();
                 //Getting names of all embedded resources
                 var pathToFile = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) + db;
+                var pathToFile2 = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) + "/common.json";
 
                 var json = File.ReadAllText(pathToFile);
+                var json2 = File.ReadAllText(pathToFile2);
 
                 List<ErrorCode> errorCodeList = JsonConvert.DeserializeObject<List<ErrorCode>>(json);
+
+                CommonWords commonWords = JsonConvert.DeserializeObject<CommonWords>(json2);
+
+                // Create a list to store the keywords
+                List<string> keywords = new List<string>();
+
+                // Iterate through error codes
+                foreach (ErrorCode errorCode in errorCodeList)
+                {
+                    // Split the code into words
+                    string[] words = errorCode.Code.Split(' ');
+                    // Add each word to the keywords list
+                    keywords.AddRange(words.Where(w => w.Length >= 4));
+                }
+
+                // Remove duplicates
+                keywords = keywords.Distinct().ToList();
+
+                // Remove common words
+                keywords = keywords.Except(commonWords.common_words).ToList();
 
                 isCommand = false;
                 string userInput = Console.ReadLine();
@@ -95,16 +117,22 @@ namespace VRChat_Error_Bot
                         {
                             Console.WriteLine("Converting to stable version...");
                             v.vType = "Stable";
-                            db = "database.json";
-                            
+                            db = "/database.json";
+                            pathToFile = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) + db;
+                            json = File.ReadAllText(pathToFile);
+                            errorCodeList = JsonConvert.DeserializeObject<List<ErrorCode>>(json);
+
                             Console.WriteLine("Current version is: " + v.v + " " + v.vType);
                         }
                         else if (versionInput == "experimental")
                         {
                             Console.WriteLine("Converting to experimental version...");
                             v.vType = "Experimental";
-                            db = "databaseex.json";
-                            
+                            db = "/databaseex.json";
+                            pathToFile = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) + db;
+                            json = File.ReadAllText(pathToFile);
+                            errorCodeList = JsonConvert.DeserializeObject<List<ErrorCode>>(json);
+
                             Console.WriteLine("Current version is: " + v.v + " " + v.vType);
                         }
                         else
@@ -133,19 +161,29 @@ namespace VRChat_Error_Bot
                 if (isCommand == false)
                 {
 
-                    if (userInput.Length < 4)
-                        userInput = "NONE";
-
                     string modifiedInput = Regex.Replace(userInput, @"'(.*?)'", "''");
                     string errorCode = Regex.Match(modifiedInput, @"\b(error|code)?\s?(\d{3})\b").Groups[2].Value;
                     var error = errorCodeList.Find(e => e.Code.IndexOf(modifiedInput, StringComparison.OrdinalIgnoreCase) >= 0 || e.Message.IndexOf(modifiedInput, StringComparison.OrdinalIgnoreCase) >= 0);
 
-                    if (error != null)
+                    foreach (string keyword in keywords)
                     {
-                        Console.Clear();
-                        Console.WriteLine(error.Response);
+                        if (error != null)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(error.Response);
+                            break;
+                        }
+                        else if (modifiedInput.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            error = errorCodeList.Find(e => e.Code.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0 || e.Message.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0);
+                            Console.Clear();
+                            Console.WriteLine("No matching error code or message was found, but keyword '" + keyword + "' was found in the input.");
+                            Console.WriteLine(error.Response);
+                            break;
+                        }
                     }
-                    else
+
+                    if (error == null)
                     {
                         Console.Clear();
                         Console.WriteLine("No matching error code or message was found.");
